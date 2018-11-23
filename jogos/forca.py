09af5104279f8472a47f32b100ed8c8f68539b57
Thiago Resende <thiago.gcresende@gmail.com>
2018-11-23 14:13:10 -0200

    game loop

--- jogos/forca.py
+++ jogos/forca.py
@@ -2,6 +2,15 @@ def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
+
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        print("Jogando")
+
     print("Fim do jogo")
 
 
