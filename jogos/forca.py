24687c5b13af62ea2d770fe64349f9d3aec8842b
Thiago Resende <thiago.gcresende@gmail.com>
2018-11-23 18:32:08 -0200

    06 - Guardando as letras acertadas

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,20 +4,22 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
+
+    print(letras_acertadas)
 
     while (not enforcou and not acertou):
         chute = input("Qual a letra? ")
         chute = chute.strip()
-
         index = 0
         for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(chute, index))
-            index = index = +1
+            if (chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+        print(letras_acertadas)
     print("Fim do jogo")
-
 
 if (__name__ == "__main__"):
     jogar()
