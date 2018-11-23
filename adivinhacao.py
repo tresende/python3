a7fe33f45c6c561df029cf027d838b0074888a9d
Thiago Resende <thiago.gcresende@gmail.com>
2018-11-22 23:28:36 -0200

    comparando variavies

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,14 @@
+print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 43
+chute = input("Digite o seu número: ")
+chute = int(chute)
+print("você digitou", chute)
+
+if(numero_secreto == chute):
+    print("Você acertou!")
+else:
+    print("Você errou!")
+print("Fim do jogo")
