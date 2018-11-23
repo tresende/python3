67210644f7cff2fe3151f1b6471fc427ba861355
Thiago Resende <thiago.gcresende@gmail.com>
2018-11-23 15:10:41 -0200

    Funções importantes da String

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,14 +3,19 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-
     palavra_secreta = "banana"
     enforcou = False
     acertou = False
 
     while (not enforcou and not acertou):
-        print("Jogando")
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
 
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(chute, index))
+            index = index = +1
     print("Fim do jogo")
 
 
