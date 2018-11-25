4d3128cfc8c21438dc23ac8e38b5f1ffda0e3389
Thiago Resende <thiago.gcresende@gmail.com>
2018-11-25 20:33:08 -0200

    6-08 Escolhendo uma palavra

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,14 +1,26 @@
+import random
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
     letras_acertadas = ["_" for letra in palavra_secreta]
     enforcou = False
     acertou = False
     erros = 0
-
+    
     print(letras_acertadas)
 
     while (not enforcou and not acertou):
