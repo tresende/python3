19afa629c52866111da98c77f2fd68a959e647c2
Thiago Resende <thiago.gcresende@gmail.com>
2018-11-25 20:53:56 -0200

    7-03 Criando mais funções

--- jogos/forca.py
+++ jogos/forca.py
@@ -13,25 +13,27 @@ def jogar():
     print(letras_acertadas)
 
     while (not enforcou and not acertou):
-        chute = input("Qual a letra? ")
-        chute = chute.strip().upper()
-        index = 0
+        chute = pede_chute()
         if(chute in palavra_secreta):
-            for letra in palavra_secreta:
-                if (chute == letra):
-                    letras_acertadas[index] = letra
-                index += 1
+            marca_chute_correto(chute, palavra_secreta, letras_acertadas)
         else:
             erros += 1
         enforcou = erros == 6
         acertou = "_" not in letras_acertadas
         print(letras_acertadas)
-    print("Fim do jogo")
-
+    if(acertou):
+        imprime_mensagem_vencedor()
+    else:
+        imprime_mensagem_perdedor()
 
 def inicializa_letras_acertadas(palavra):
     return ["_" for letra in palavra]
 
+def imprime_mensagem_vencedor():
+    print("Você ganhou!")
+
+def imprime_mensagem_perdedor():
+    print("Você perdeu!")
 
 def carrega_palavra_secreta():
     arquivo = open("palavras.txt", "r")
@@ -49,6 +51,20 @@ def imprime_mensagem_abertura():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
+
+
+def pede_chute():
+    chute = input("Qual a letra? ")
+    chute = chute.strip().upper()
+    return chute
+
+
+def marca_chute_correto(chute, palavra_secreta, letras_acertadas):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute == letra):
+            letras_acertadas[index] = letra
+        index += 1
 
 
 if (__name__ == "__main__"):
