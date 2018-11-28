74caa129b2230420dafbda3edd28c8abd996c67c
Thiago Resende <thiago.gcresende@gmail.com>
2018-11-28 14:55:21 -0200

    clean code

--- oo/conta.py
+++ oo/conta.py
@@ -4,6 +4,31 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+        self.__codigo_banco = "001"
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco(self):
+        return "001"
+    
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
 
     def extrato(self):
         print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
@@ -23,24 +48,7 @@ class Conta:
 
     def transfere(self, valor, destino):
         self.saca(valor)
-        destino.deposita(valor)
-
-    @property
-    def saldo(self):
-        return self.__saldo
-
-    @property
-    def titular(self):
-        return self.__titular
-
-    @property
-    def limite(self):
-        return self.__limite
-
-    @limite.setter
-    def limite(self, limite):
-        self.__limite = limite
-
+        destino.deposita(valor)        
 
 if (__name__ == "__main__"):
     conta = Conta('037112', 'Thiago', 200, 300)
