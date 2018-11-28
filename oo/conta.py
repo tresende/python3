58b92854bb8efd501a1b955d8cb2e04ee9cdeb87
Thiago Resende <thiago.gcresende@gmail.com>
2018-11-28 12:17:37 -0200

    Atibutos privados

--- oo/conta.py
+++ oo/conta.py
@@ -1,21 +1,27 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
 
 if (__name__ == "__main__"):
     conta = Conta('037112', 'Thiago Resende', 200, 300)
+
+    conta2 = conta
+    conta2.__saldo = 99999999
+    conta2 = None
+    
     conta.extrato()
     conta.deposita(15.0)
     conta.extrato()
