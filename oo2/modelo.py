742d0c901f0e7e4690fc27db7801c5e20af1910e
Thiago Resende <thiago.gcresende@gmail.com>
2018-11-30 14:50:33 -0200

    1-06: Encapsulando comportamento

--- oo2/modelo.py
+++ oo2/modelo.py
@@ -1,26 +1,53 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
     def dar_like(self):
-        self.likes += 1
-    
+        self.__likes += 1
+
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
+
 
 vingadores = Filme('vingadores guerra infinita', 2018, 160)
 atlanta = Serie('Atlanta', 2017, 2)
+
+atlanta.nome = 'Atlanta 2'
 
 atlanta.dar_like()
 atlanta.dar_like()
